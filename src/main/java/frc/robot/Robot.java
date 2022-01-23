f75de84c73f16240162109574ed8468d02e553c5
bambam7480 <neutrino314@outlook.com>
2022-01-22 19:30:50 -0500

    New code that should actually work, though we're not completely sure because Fred was not cooperating today. The code uses logitech joysticks for a basic tankdrive setup.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,40 +4,40 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
  * the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
+
+  private DriveTrain m_myRobot;
   private Joystick m_leftStick;
   private Joystick m_rightStick;
 
-  // change ports, change pwm to can
-  private final MotorController m_leftMotorFront = new PWMSparkMax(0);
-  private final MotorController m_leftMotorRear = new PWMSparkMax(1);
-
-  SpeedControllerGroup m_left = new SpeedControllerGroup(m_leftMotorFront, m_leftMotorRear)
-
-  private final MotorController m_rightMotorFront = new PWMSparkMax(2);
-  private final MotorController m_rightMotorRear = new PWMSparkMax(3);
   
-  SpeedControllerGroup m_right = new SpeedControllerGroup(m_rightMotorFront, m_rightMotorRear)
+
+
+  // MotorControllerGroup m_left = new MotorControllerGroup(m_leftMotorFront, m_leftMotorRear);
+
+ 
+  // MotorControllerGroup m_right = new MotorControllerGroup(m_rightMotorFront, m_rightMotorRear);
 
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_right.setInverted(true);
+    
 
-    m_myRobot = new DifferentialDrive(m_left, m_right);
+
+    m_myRobot = new DriveTrain();
     //joystick ports might need to be changed
     m_leftStick = new Joystick(0);
     m_rightStick = new Joystick(1);
